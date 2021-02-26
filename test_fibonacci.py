6795f700e22b8216f433c9a8deed05246752612f
alexgpitts <alexgpitts@gmail.com>
2021-02-25 19:42:49 -0800

    Had the test in the wrong file

---
+++ test_fibonacci.py
@@ -0,0 +1,13 @@
+import unittest
+import fibonacci
+
+class TestCase(unittest.TestCase):
+
+    def test_fib_9(self):
+        self.assertEqual(fibonacci.fib(9), 34)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
