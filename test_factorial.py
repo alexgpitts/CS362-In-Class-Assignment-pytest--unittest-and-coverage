662a8efa79722d428756073211eeb61719db73c9
alexgpitts <alexgpitts@gmail.com>
2021-02-25 20:39:55 -0800

    Tried to use coverage. Will pick up next week

--- test_factorial.py
+++ test_factorial.py
@@ -3,16 +3,13 @@ import unittest
 import fibonacci
 
 
-#unittest class tests
-class TestCase(unittest.TestCase):
-    def test_fac_9(self):
-        self.assertEqual(fibonacci.factorial(9), 362880) 
-    def test_fac_0(self): 
-        self.assertEqual(fibonacci.factorial(0), 1)
-    def test_fac5(self): 
-        self.assertEqual(fibonacci.factorial(5), 120)    
 
+def test_fac_9():
+    assert fibonacci.factorial(9) == 362880
 
+def test_fac_0():
+    assert fibonacci.factorial(0) == 1 
 
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+def test_fac5():
+    assert fibonacci.factorial(5) == 120
+
