115508ab2a4bdb5d4c7f3975556fc62c07b08a20
alexgpitts <alexgpitts@gmail.com>
2021-02-25 20:00:39 -0800

    fixed implementation a little bit

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,15 @@
 def fib(nthNum):
     j =0 
-    k=1 
+    k=0 
     fib = 0
 
-    for i in range(nthNum-1):
+    for i in range(nthNum):
         fib = j+k
         j = k
-        k = fib
+        if(i == 0):
+            k +=1
+        else:
+            k = fib
 
     return fib
             
